9b30f4824f51da265baa5131a85c568c7446137b
essonani tchakpalla <essonani@hotmail.com>
2023-06-16 18:51:35 -0500

    class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new car();
+            myCar.Make = "Ford";
+            myCar.Model = "Taurus";
+            myCar.Year = "2013";
+            var mazda = new car()
+            {
+                Make = "mazda" ,
+                Model="cx-5",
+                Year="2010",
+
+            };
+            var chevy = new car("chevy", "malibu", 2015);
+            var carlist = new List<car>() { myCar, chevy,mazda,new car() };
+           foreach(var vehicule in carlist)
+            {
+                Console.WriteLine( $" {vehicule.Make}    {vehicule.Model}  {   vehicule.Year}");
+            }
         }
     }
+
+   
 }
---
+++ Classes/car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public  class car
+    { 
+        public car() 
+        { 
+
+        }
+
+        public car (string makeInput,string modelInput,int yearInput)
+        {
+          Make=makeInput;
+          Model=modelInput;
+            Year = Year;
+        }
+            
+      public string Make { get; set; }
+        public string Model { get; set; }    
+        public string Year { get; set; }
+
+    }
+}
